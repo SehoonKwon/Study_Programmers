06f63696d6623422f937b3e6734214f4360d8e76
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2021-01-04 22:08:02 +0900

    Create PG_문자열 내림차순 정렬.cpp

---
+++ PG_문자열 내림차순 정렬.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    sort(s.begin(), s.end(), greater<int>());
+    return s;
+}
